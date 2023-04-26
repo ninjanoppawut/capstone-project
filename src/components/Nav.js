9842e406ee809b50dd93cd0923ab1794fee58059
ninjanoppawut <ninja.noppawut@gmail.com>
2023-04-26 15:29:14 +0700

    Navbar added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './Nav.css';
 
 function Nav() {
   return (
-    <nav>
-        <img src='./logo1.png' alt='logo'></img>
-        <ul>
+    <nav >
+        <ul className='nav-items'>
+          <div className='logo-container'>
+            <img src='./logo1.png' alt='logo'></img>
+          </div>
           <li><a href="home">Home</a></li>
           <li><a href="about">About</a></li>
           <li><a href="menu">Menu</a></li>
